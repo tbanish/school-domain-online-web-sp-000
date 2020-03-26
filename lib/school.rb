249e712bc6a2521d6c0d72a07909d46c08660d41
Tom Banish <tombanish@gmail.com>
2020-03-26 12:57:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,9 @@ class School
   end
 
   def add_student(student_name, grade)
+    if @roster[grade] == true
+      @roster[grade] << student_name
+    else
     @roster[grade] = []
     @roster[grade] << student_name
   end 
