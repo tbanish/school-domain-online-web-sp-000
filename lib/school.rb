5aafe121abc8ac977e7b4e99c7edc3403fd7e2f9
Tom Banish <tombanish@gmail.com>
2020-03-26 12:51:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
   end
 
   def add_student(student_name, grade)
-    @roster[student_name] = []
-    @roster[student_name] << grade
+    @roster[grade] = []
+    @roster[grade] << student_name
   end 
 
   
